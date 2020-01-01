7daf8de7efd2da0e17dfa3bdc828a216816401ba
unknown <thddudwn1012@naver.com>
2020-01-01 21:04:56 +0900

    main screen tab bar 추가

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Login from './src/screen/Login';
 import Join from './src/screen/Join';
 import Main from './src/screen/Main';
+import Setting from './src/screen/Setting';
  
 const AppNavigator = createStackNavigator(
   {
     Logins: Login,
     Joins: Join,
     Mains : Main, 
+//    Settings : Setting, 
   },
   { 
     initialRouteName: 'Logins',
---
+++ src/screen/Setting.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import styles from "../components/loginStyle";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+class SettingsScreen extends React.Component {
+    render() {
+      return (
+          <Text>Settings!</Text>
+      );
+    }
+  }
+  
+
+export default SettingsScreen;
