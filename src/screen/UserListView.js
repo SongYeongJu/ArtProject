2930fd8021fe0dfb476646fd3f4840fec261398e
unknown <thddudwn1012@naver.com>
2020-01-07 23:39:53 +0900

    like list view 추가

--- src/screen/UserListView.js
+++ src/screen/UserListView.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { View } from "react-native";
+import { View, Text, Image, FlatList, TouchableOpacity} from "react-native";
+import styles from "../components/userlistStyle";
+import datas from '../datas/data2';
 
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
 
 class UserListView extends React.Component {
-    render() {
+      render() {
       return (
-          <View>
-              <Text>user list view</Text>
-          </View>
+        <View style={styles.container}>
+            <Text style={styles.titleText}> Like List </Text>
+        <FlatList style={styles.container2}
+            data={datas}
+            renderItem={({ item }) => {
+              return (
+              <TouchableOpacity>
+                  <Image 
+                    style={styles.im} 
+                    source={{uri:item.uri}}/>
+                <Text style={styles.ttext}>{item.name}</Text>
+                <Text>{item.artist}</Text>
+                    </TouchableOpacity>  
+              );      
+            }
+          }
+        />
+      </View>
+
       );
     }
   }
-  
-export default UserListView;
+
+  export default UserListView;
