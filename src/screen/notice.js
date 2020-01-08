ccba4b9e19e151d14245ea5d7335b95320840b66
unknown <quminseok123@gmail.com>
2020-01-08 16:36:18 +0900

    SDASD

--- src/screen/notice.js
+++ src/screen/notice.js
@@ -1,128 +1,132 @@
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React, {Component} from 'react';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Text, View, TextInput, ScrollView, TouchableOpacity,StyleSheet } from "react-native";
+import NoticeNote from './NoticeNote';
 import { Ionicons } from "@expo/vector-icons";
-import { createAppContainer } from 'react-navigation';
 
-import Main from './Main';
-import Tab2 from './Tab2';
-import Tab3 from './Tab3';
-import Setting from './Setting';
-import Splash from './Splash';
+export default class notice extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      noteArray: [],
+      noteText: '',
+    }
+  }
 
-export default class SettingListView extends Component {
- 
   render() {
- 
+
+    let notes = this.state.noteArray.map((val,key)=>{
+      return <NoticeNote key={key} keyval={key} val={val}
+              deleteMethod={ ()=> this.deleteNote(key)} />
+    });
     return (
- 
-      <View style={styles.MainContainer}>
- 
-        <Text style={{ marginTop: 40, fontSize: 20 }}>Settings Activity Screen</Text>
- 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.push('Tab1')}>
- 
-          <Text style={styles.text}>개인정보</Text>
- 
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.push('Tab1')}>
- 
-          <Text style={styles.text}>공지사항</Text>
- 
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.push('Tab1')}>
- 
-          <Text style={styles.text}>포인트</Text>
- 
-        </TouchableOpacity>
- 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.push('Settings')}>
- 
-          <Text style={styles.text}>문의 및 건의</Text>
- 
-        </TouchableOpacity>
- 
+      <View style = {styles.container}>
+        <View style={styles.titleBar}>
+              <TouchableOpacity onPress={() => this.props.navigation.push('SettingListViews')}>
+                  <Ionicons name="ios-arrow-back" size={24} color="#000000"></Ionicons>
+              </TouchableOpacity>
+        </View>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> -Notice-</Text>
+        </View>
+        
+        <ScrollView style={styles.scrollContainer}>
+          {notes}
+        </ScrollView>
+
+        <View style={styles.footer}>
+
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(noteText)=>this.setState({noteText})}
+            value={this.state.noteText}
+            placeholder='>notice'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'>
+          </TextInput>
       </View>
- 
+      <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+      <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+      </View>
     );
   }
-}
-
-const TabNavigator = createBottomTabNavigator({
-  Home: Main,
-  Tab2: Tab2,
-  Tab3: Tab3,
-  profile : Setting,
-  Splashs: Splash,
-
-},
-
-{
-  defaultNavigationOptions: ({ navigation }) => ({
-    tabBarIcon: ({ focused, horizontal, tintColor, image }) => {
-      const { routeName } = navigation.state;
-      let iconName;
-      if (routeName === "Home") {
-        iconName = "ios-home";
-      } else if (routeName === "Tab2") {
-        iconName = "ios-person";
-      } else if(routeName === "Tab3") {
-          iconName = "ios-person";
-      } else if (routeName === "Splashs") {
-        iconName = "ios-chatboxes";
-      } else if (routeName === "profile") {
-        iconName = "ios-person";
-      }
-
-      return (
-        (
-          <Ionicons
-            name={iconName}
-            size={horizontal ? 20 : 25}
-            color={tintColor}
-          />
-        )
-      );
-    },
-    tabBarOptions: {
-      activeTintColor: "#74b9ff",
-      inactiveTintColor: "gray"
+  addNote(){
+    if(this.state.noteText){
+      let d=new Date();
+      this.state.noteArray.push({
+        'date':d.getFullYear()+
+        "/"+(d.getMonth()+1)+
+        "/"+d.getDate(),
+        'note':this.state.noteText
+      });
+      this.setState({noteArray: this.state.noteArray});
+      this.setState({noteText: ''});
     }
-  })
-}
-);
-const styles = StyleSheet.create({
- 
-  MainContainer: {
- 
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-    padding: 11
- 
-  },
- 
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#43A047',
-    padding: 12,
-    width: 280,
-    marginTop: 12,
-  },
- 
-  text: {
- 
-    color: '#fff'
   }
-  
- 
-});
+
+  deleteNote(key){
+    this.state.noteArray.splice(key,1);
+    this.setState({noteArray: this.state.noteArray});
+  }
+
+}
+
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+    },
+    titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 70,
+        marginHorizontal: 16
+    },
+    header: {
+      backgroundColor: '#000000',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderBottomWidth:10,
+      borderBottomColor: '#ddd',
+    },
+    headerText: {
+      color: 'white',
+      fontSize: 18,
+      padding:  26,
+    },
+    scrollContainer:{
+      flex:1,
+      marginBottom: 100,
+    },
+    footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 10,
+    },
+    textInput: {
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#252525',
+      borderTopWidth:2,
+      borderTopColor:'#ededed',
+    },
+    addButton: {
+      position: 'absolute',
+      zIndex:11,
+      right:20,
+      bottom: 90,
+      backgroundColor: '#000000',
+      width:90,
+      height:90,
+      borderRadius: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      elevation: 8,
+    },
+    addButtonText: {
+      color: '#fff',
+      fontSize: 24,
+    }
+  });
