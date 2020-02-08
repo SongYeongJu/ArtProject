630cbe37552ef8560bdeb942ead8577b96fb805c
unknown <thddudwn1012@naver.com>
2020-02-09 04:17:45 +0900

    feed category button 추가

--- src/screen/Feed.js
+++ src/screen/Feed.js
@@ -1,50 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import moment from "moment";
 import headerStyle from '../components/headerStyle';
+import postDatas from '../datas/feedData';
+
+import styles from '../components/feedStyle';
 
 // temporary data until we pull from db
-const posts = [
-    {
-        id: "1",
-        name: "Joe McKay",
-        text:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        timestamp: 1569109273726,
-        avatar: require("../image/ba1.jpg"),
-        image: require("../image/ba2.jpg")
-    },
-    {
-        id: "2",
-        name: "Karyn Kim",
-        text:
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        timestamp: 1569109273726,
-        avatar: require("../image/ba1.jpg"),
-        image: require("../image/ba2.jpg")
-    },
-    {
-        id: "3",
-        name: "Emerson Parsons",
-        text:
-            "Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant.",
-        timestamp: 1569109273726,
-        avatar: require("../image/ba1.jpg"),
-        image: require("../image/ba2.jpg")
-    },
-    {
-        id: "4",
-        name: "Kathie Malone",
-        text:
-            "At varius vel pharetra vel turpis nunc eget lorem. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Adipiscing tristique risus nec feugiat in fermentum.",
-        timestamp: 1569109273726,
-        avatar: require("../image/ba1.jpg"),
-        image: require("../image/ba2.jpg")
-    }
-];
-
-export default class HomeScreen extends React.Component {
+export default class FeedScreen extends React.Component {
     renderPost = post => {
         return (
           //  <Text style={styles.timestamp}>{moment(post.timestamp).fromNow()}</Text> Line 56
@@ -76,9 +39,17 @@ export default class HomeScreen extends React.Component {
                 <View style={headerStyle.header}> 
                     <Image style={headerStyle.headerImage} source={require('../image/color_logo.png')}/>
                 </View>
+                <View style={styles.container2}> 
+                <TouchableOpacity style={styles.cateButton}>
+                        <Text style={styles.cateText}>Posts</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cateButton}>
+                        <Text style={styles.cateText}>Chat</Text>
+                    </TouchableOpacity>
+                </View>
                 <FlatList
                     style={styles.feed}
-                    data={posts}
+                    data={postDatas}
                     renderItem={({ item }) => this.renderPost(item)}
                     keyExtractor={item => item.id}
                     showsVerticalScrollIndicator={false}
@@ -87,67 +58,3 @@ export default class HomeScreen extends React.Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#EBECF4"
-    },
-    header: {
-        width:'100%',
-        height: '13%',
-        paddingBottom:'2%',
-        backgroundColor: "#FFF",
-        alignItems: "flex-end",
-        justifyContent: "flex-end",
-        // borderBottomWidth: 1,
-        // borderBottomColor: "#EBECF4",
-        // shadowColor: "#454D65",
-        // shadowOffset: { height: 5 },
-        // shadowRadius: 15,
-        // shadowOpacity: 0.2,
-        zIndex: 10
-    },
-    headerImage : {
-      width: '100%',
-      height:'33%',
-      resizeMode:'contain',
-    },
-    feed: {
-        marginHorizontal: 16
-    },
-    feedItem: {
-        backgroundColor: "#FFF",
-        borderRadius: 5,
-        padding: 8,
-        flexDirection: "row",
-        marginVertical: 8
-    },
-    avatar: {
-        width: 36,
-        height: 36,
-        borderRadius: 18,
-        marginRight: 16
-    },
-    name: {
-        fontSize: 15,
-        fontWeight: "500",
-        color: "#454D65"
-    },
-    timestamp: {
-        fontSize: 11,
-        color: "#C4C6CE",
-        marginTop: 4
-    },
-    post: {
-        marginTop: 16,
-        fontSize: 14,
-        color: "#838899"
-    },
-    postImage: {
-        width: undefined,
-        height: 150,
-        borderRadius: 5,
-        marginVertical: 16
-    }
-});
